272e66f55fb18b7a7ed799527e653357069242a4
wuluchaoren <38044760+wuluchaoren@users.noreply.github.com>
2019-05-02 01:08:39 +0800

    【更新】页面样式的调整
    2019/5/2 1:08

--- src/Page/Extraction.vue
+++ src/Page/Extraction.vue
@@ -1,13 +1,46 @@
 <template>
-    
+  <div class="root">
+    <div class="head">
+      <MyHead text="信息抽取"></MyHead>
+    </div>
+    <div class="main">
+      <ExtractionBody></ExtractionBody>
+    </div>
+    <div class="foot">
+      <ExtractionButton></ExtractionButton>
+    </div>
+  </div>
 </template>
 
 <script>
+    import MyHead from "../components/MyHead";
+    import ExtractionButton from "../components/ExtractionButton";
+    import ExtractionBody from "../components/ExtractionBody";
     export default {
-        name: "Extraction"
+      name: "Extraction",
+      components: {ExtractionBody, ExtractionButton, MyHead },
+
     }
 </script>
 
 <style scoped>
-
+  .root{
+    height: 100%;
+    width: 100%;
+  }
+  .head{
+    height: 7%;
+    width: 100%;
+  }
+  .main{
+    width: 100%;
+    height: 88%;
+  }
+  .foot{
+    width: 100%;
+    height: 5%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
--- src/Page/Result.vue
+++ src/Page/Result.vue
@@ -1,13 +1,44 @@
 <template>
-    
+  <div class="root">
+    <div class="head">
+      <MyHead text="抽取结果"></MyHead>
+    </div>
+    <div class="main">
+      <ResultBody></ResultBody>
+    </div>
+    <div class="foot">
+      <BackButton></BackButton>
+    </div>
+  </div>
 </template>
 
 <script>
+  import MyHead from "../components/MyHead";
+  import BackButton from "../components/BackButton";
+  import ResultBody from "../components/ResultBody";
     export default {
-        name: "Result"
+        name: "Result",
+        components: {ResultBody, BackButton, MyHead },
     }
 </script>
 
 <style scoped>
-
+  .root{
+    height: 100%;
+    width: 100%;
+  }
+  .head{
+    height: 7%;
+    width: 100%;
+  }
+  .main{
+    height: 88%;
+    width: 100%;
+  }
+  .foot{
+    width: 100%;
+    height: 5%;
+    display: flex;
+    justify-content: center;
+  }
 </style>
