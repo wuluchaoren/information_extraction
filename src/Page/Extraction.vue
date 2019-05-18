a2fa7b2c1a1471d72c971f7cd43b470a0a9d369f
wuluchaoren <38044760+wuluchaoren@users.noreply.github.com>
2019-05-18 13:52:28 +0800

    【新增】业务代码编写
    2019/5/18 13:52

--- src/Page/Extraction.vue
+++ src/Page/Extraction.vue
@@ -4,11 +4,11 @@
       <MyHead text="音乐家生平信息抽取"></MyHead>
     </div>
     <div class="main">
-      <ExtractionBody></ExtractionBody>
+      <ExtractionBody ref="child"></ExtractionBody>
     </div>
     <div class="foot">
       <div class="foot_left"></div>
-      <ExtractionButton></ExtractionButton>
+      <ExtractionButton v-on:extract="onclick"></ExtractionButton>
     </div>
   </div>
 </template>
@@ -17,10 +17,54 @@
     import MyHead from "../components/MyHead";
     import ExtractionButton from "../components/ExtractionButton";
     import ExtractionBody from "../components/ExtractionBody";
+    import qs from 'qs';
     export default {
       name: "Extraction",
-      components: {ExtractionBody, ExtractionButton, MyHead },
-
+      components: {ExtractionBody, ExtractionButton, MyHead},
+      data() {
+        return {
+          content: '',
+          file: null
+        }
+      },
+      methods: {
+        onclick: function () {
+          let _this = this;
+          let child = this.$refs.child;
+          _this.$data.content = child.content;
+          _this.$data.file=child.file;
+          console.log(this.$data.content);
+          _this.$data.content===''?_this.extractFile():_this.extractContent();
+        },
+        extractContent: function () {
+          let _this = this;
+          _this.$axios.post('/text', {text:_this.$data.content}).then((res) => {
+            localStorage.setItem('response', JSON.stringify([].slice.call(res.data)));
+            this.$router.push({
+              name: 'Result'
+            });
+          }).catch((err) => {
+            console.log(err);
+          })
+        },
+        extractFile: function() {
+          let _this = this;
+          const myConfig={
+            headers: {'Content-Type': 'multipart/form-data'}
+          };
+          const formdata=new FormData();
+          formdata.append('file',_this.$data.file);
+          console.log(formdata.get('file'));
+          _this.$axios.post('/file', formdata, myConfig).then((res) => {
+            localStorage.setItem('response', JSON.stringify([].slice.call(res.data)));
+            this.$router.push({
+              name: 'Result'
+            });
+          }).catch((err) => {
+            console.log(err);
+          })
+        }
+      }
     }
 </script>
 
