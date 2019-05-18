180c47258904cfbf27de76c4cde3db93571443dd
wuluchaoren <38044760+wuluchaoren@users.noreply.github.com>
2019-05-18 13:52:01 +0800

    【更新】配置插件和依赖项
    2019/5/18

--- src/main.js
+++ src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$axios = axios
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  created(){
+    axios.defaults.baseURL='http://119.29.24.35:8002/'
+  },
   components: { App },
-  template: '<App/>'
+  template: '<App/>',
 })
